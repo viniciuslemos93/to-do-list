910d1a0886a8275dcc90d4ae37c418d77a916ab4
Vinicius Lemos <vinycius1993@gmail.com>
2023-03-22 13:53:07 -0300

    feat: implementando a função para adicionar as tasks

--- src/components/TodoForm/index.js
+++ src/components/TodoForm/index.js
@@ -1,14 +1,20 @@
 import './TodoForm.css'
 
+
 import React from 'react'
 
 const TodoForm = () =>  {
+  const addTask = () => {
+    console.log('Teste Função')
+    
+  }
+
   return (
     <div>
         <h1>To Do List</h1>
         <div>
           <input className='addTask shadow' type="text" />
-          <button className='btnAddTask shadow'>Adicionar</button>
+          <button className='btnAddTask shadow' onClick={addTask}>Adicionar</button>
         </div>
     </div>
   )
